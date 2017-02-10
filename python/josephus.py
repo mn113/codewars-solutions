8dd5ff3c11514668ee84c47f1ffd5dc8ba5a041c
Martin Nicholson <recyclebing+github@gmail.com>
2017-02-10 20:47:37 +0000

    Josephus 1 more method

--- python/josephus.py
+++ python/josephus.py
@@ -24,10 +24,23 @@ def josephus1(total,k):
     return men.popitem()[0]
 
 
+# List-slicing method:
+def josephus2(total,k):
+    men = list(range(1,total+1))
+    i = -1
+    while len(men) > 1:
+        i = (i + k) % len(men)
+        men = men[:i] + men[i+1:]
+        i -= 1
+
+    return men[0]
+
+
 print josephus1(7,3)     # 4
 print josephus1(11,19)   # 10
 print josephus1(1,300)   # 1
 print josephus1(14,2)    # 13
 print josephus1(100,1)   # 100
 #print josephus0(2349,2325)  # exceeds max recusion depth
-print josephus1(2349,2325)  # 924 locally   # exceeds 12000ms on CodeWars
+#print josephus1(2349,2325)  # 924 locally   # exceeds 12000ms on CodeWars
+print josephus2(2349,2325)  # 924
